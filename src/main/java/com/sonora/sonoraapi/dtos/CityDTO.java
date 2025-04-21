517e28b7ab336316abe1593ac88e5566e51e50bb
Leandro Cunha <leandrocunha016@gmail.com>
2025-04-21 20:16:15 -0300

    chore: Remove unused import in CityDTO
    The unused import for `State` has been removed from `CityDTO`. This cleanup improves code readability and eliminates unnecessary dependencies.

--- src/main/java/com/sonora/sonoraapi/dtos/CityDTO.java
+++ src/main/java/com/sonora/sonoraapi/dtos/CityDTO.java
@@ -1,6 +1,5 @@
 package com.sonora.sonoraapi.dtos;
 
-import com.sonora.sonoraapi.entities.State;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
